d4256e00a9ec7aa77825a72e38e215de83b91152
marcodavidg <marcodavidg@gmail.com>
2023-10-26 06:30:23 +0800

    Initial commit with working DQN

---
+++ deepq_network.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+import random
+
+# Define your custom loss function
+def custom_loss(target_Qs, Qs):
+    return tf.reduce_mean(tf.square(target_Qs - Qs))
+
+class DQN():
+    def __init__(self, state_size, possible_actions, name):
+        # super(DQN, self).__init__()
+        self.possible_actions = possible_actions
+        self.action_size = len(possible_actions)
+        self.name = name
+        self.target_Q = None # target_Q is the R(s,a) + ymax Qhat(s', a')
+
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(state_size,)),  # Input layer with 4 features
+            tf.keras.layers.Dense(128, activation='relu'),  # First hidden layer with 128 units and ReLU activation
+            tf.keras.layers.Dense(64, activation='relu'),  # Second hidden layer with 64 units and ReLU activation
+            tf.keras.layers.Dense(self.action_size, activation='softmax')  # Output layer with 10 units and softmax activation
+        ])
+
+        self.model.compile(optimizer='adam', loss=custom_loss, metrics=['accuracy'])
+
+    def calculate_loss(self, target_Qs, states_mb, actions_mb):
+        with tf.GradientTape() as tape:
+            Qs = tf.reduce_sum(tf.multiply(self.model(states_mb), actions_mb), axis=1)
+            loss = custom_loss(target_Qs, Qs)
+
+        # Calculate gradients
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+
+        # Apply gradients to update model parameters
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)  # You can adjust the learning rate
+        optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        # print(f"Epoch {epoch + 1}, Loss: {loss.numpy()}")
+        return loss
+
+
+    def predict_action(self, explore_start, explore_stop, decay_rate, decay_step, state):
+        # exploration - exploitation tradeoff
+        # breakpoint()
+        exp_exp_tradeoff = np.random.rand()
+        possible_actions = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+        # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
+        explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+
+        if explore_probability > exp_exp_tradeoff:
+            # Choose a random action (exploration)
+            action = random.choice(possible_actions)
+            # print("RANDOM", action)
+        else:
+            # Get action from Q-network (exploitation)
+            # Estimate the Qs values state
+            # Qs = sess.run(DeepQNetwork.output, feed_dict={DeepQNetwork.inputs_: state.reshape((1, *state.shape))})
+            # breakpoint()
+            output = self.model(state[np.newaxis, :])
+            # Take the biggest Q value (= the best action)
+            choice = np.argmax(output)
+            action = possible_actions[int(choice)]
+            # print("PREDICTION", action)
+
+        return action, explore_probability
+
+
+
+
+
+
---
+++ environment.py
@@ -0,0 +1,95 @@
+import tensorflow as tf      # Deep Learning library
+import numpy as np           # Handle matrices
+import random
+import matplotlib.pyplot as plt # Display graphs
+import math
+import os
+import pandas
+
+import deepq_network
+import memory
+class Environment:
+    def __init__(self, reward_pos):
+        self.wins = 0
+        self.losses = 0
+        self.points_lost = -100
+        self.points_reward = 1000
+        self.size = np.array([5,5])
+        self.reward_pos = np.array(reward_pos)
+        self.board = self.get_new_board()
+        self.pos = np.array([1, 1])
+        self.printing = False
+
+    def reset(self):
+        self.board = self.get_new_board()
+        self.pos = np.array([1, 1])
+
+
+    def get_new_board(self):
+        board = np.zeros((self.size[0] + 1, self.size[1] + 1))
+        board[0, :] = self.points_lost
+        board[-1, :] = self.points_lost
+        board[:, 0] = self.points_lost
+        board[:, -1] = self.points_lost
+        board[self.reward_pos[0], self.reward_pos[1]] = self.points_reward
+        return board
+
+    def left(self):
+        self.pos[1] -= 1
+
+    def right(self):
+        self.pos[1] += 1
+
+    def down(self):
+        self.pos[0] += 1
+
+    def up(self):
+        self.pos[0] -= 1
+
+    def move(self, move):
+        a = self.pos.copy()
+        if move[0] == 1:
+            self.pos[0] -= 1
+        elif move[1] == 1:
+            self.pos[0] += 1
+        elif move[2] == 1:
+            self.pos[1] -= 1
+        elif move[3] == 1:
+            self.pos[1] += 1
+        reward = self.get_reward()
+        # print("moved:", move, "Estaba en ", a[0], a[1], "y ahora en", self.pos[0], self.pos[1])
+        if self.printing:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            self.draw()
+        return reward, self.pos.copy()
+
+    def draw(self):
+        x = self.board.copy()
+        x[self.pos[0], self.pos[1]] = 7
+        print(x)
+
+    def get_state(self):
+        return self.pos
+
+    def get_current_value(self):
+        return self.board[self.pos[0], self.pos[1]]
+
+    def get_reward(self):
+        # Return Current reward - Euclidean distance to reward
+        return self.get_current_value() - np.linalg.norm(self.pos - self.reward_pos)
+
+    def is_finished(self):
+        finished = (self.get_current_value() != 0)
+        if finished:
+            self.printing = False
+            if self.get_current_value() == self.points_lost:
+                self.losses += 1
+            else:
+                self.wins += 1
+        return finished
+
+    def get_ratio(self):
+        return (self.wins + 1) / (self.losses + 1)
+
+    def print_next(self):
+        self.printing = True
